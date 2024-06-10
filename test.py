1807a1ac330dceb3f40bd2ea156c1f27d676a7b3
natekrth <natekrth@gmail.com>
2024-06-10 14:50:54 +0900

    another contrast way

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import pydicom as dicom
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QToolBar, QAction, QWidget, QSlider, QLabel, QSplitter, QGraphicsView, QGraphicsScene, QHBoxLayout, QGridLayout, QSizePolicy, QMenu, QFileDialog, QListWidget, QListWidgetItem
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage, QPixmap, QColor
+from PIL import Image
 
 class Vector3D:
     def __init__(self, x, y, z):
@@ -346,26 +347,31 @@ class MainPage(QMainWindow):
         #     return
         self.display_images(self.selectedItem.image_data)
 
-    def make_2d_image(self, image_2d):
+    def make_2d_image(self, image_2d, contrast_factor=1):
         # Normalize the image data
         normalized_image = ((image_2d - image_2d.min()) / (image_2d.max() - image_2d.min()) * 255).astype(np.uint8)
-        
+
+        # Adjust contrast
+        factor = (259 * (contrast_factor + 255)) / (255 * (259 - contrast_factor))
+        adjusted_image = np.clip(128 + factor * (normalized_image - 128), 0, 255).astype(np.uint8)
+
         # Set the background to black where pixel values are above a certain threshold (e.g., 250)
         threshold = 250
-        background_mask = normalized_image > threshold
-        normalized_image[background_mask] = 0
-        
+        background_mask = adjusted_image > threshold
+        adjusted_image[background_mask] = 0
+
         # Create QImage
-        height, width = normalized_image.shape
-        image_2d_bytes = normalized_image.tobytes()
+        height, width = adjusted_image.shape
+        image_2d_bytes = adjusted_image.tobytes()
         image = QImage(image_2d_bytes, width, height, QImage.Format_Grayscale8)
         return image
+
 
     def get_image_position(slice):
         return slice.ImagePositionPatient[2]
 
     def load_dicom_images(self):
-        path = "./pA"
+        path = "./pB"
         ct_images = os.listdir(path)
         slices = [dicom.read_file(path + '/' + s, force=True) for s in ct_images]
         slices = sorted(slices, key=lambda x: x.ImagePositionPatient[2], reverse=True)
